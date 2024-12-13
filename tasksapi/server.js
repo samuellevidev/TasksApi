5701a0b85fea7fd629e3c9a19130f6753c23a277
Samuel Levi <113398240+samuellevidev@users.noreply.github.com>
2024-12-13 07:53:23 -0300

    Delete tasksapi/server.js

--- tasksapi/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const userRoutes = require('./src/routes/userRoutes');
-const taskRoutes = require('./src/routes/taskRoutes');
-const categoryRoutes = require('./src/routes/categoryRoutes');
-
-const app = express();
-app.use(express.json());
